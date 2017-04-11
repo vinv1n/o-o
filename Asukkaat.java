81ed0adebccbd81bff6455e1b4279fcce73750d8
Juho Sipola <jusipola@student.oulu.fi>
2017-04-11 15:52:41 +0300

    Create Asukkaat.java

---
+++ Asukkaat.java
@@ -0,0 +1,18 @@
+public class Asukkaat{
+	
+	private String nimi;
+	private int ika;
+	
+	public void set_nimi(String nimi){
+		this.nimi = nimi;
+	}
+	public void set_ika(int ika){
+		this.ika = ika;
+	}
+	public String get_nimi(){
+		return nimi;
+	}
+	public int get_ika(){
+		return ika;
+	}
+}

503fd949727f72ced7991d6d895ed73aedb63fd3
Juho Sipola <jusipola@student.oulu.fi>
2017-04-20 23:45:19 +0300

    Create Subscription.java

---
+++ Subscription.java
@@ -0,0 +1,11 @@
+public class Subscription {
+
+	public String lehti;
+	public String tilaaja;
+	public  String osoite;
+	public  double kuukausihinta;
+	
+	void printInvoice(){
+		//kutsuu regular tai standing classia
+	}
+}
